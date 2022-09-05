d673adf05f1853cceeb0104a814f674ec7c5f953
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-05 09:05:54 -0500

    primer commit

---
+++ src/main/java/pe/edu/pucp/AdviertePucpApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviertePucpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdviertePucpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/AdviertePucpApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviertePucpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
